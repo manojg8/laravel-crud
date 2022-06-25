51bd03eeb08e42e5b1eec130cd3364ed71e29c48
Manoj Gonte <manoj@ycstech.in>
2022-06-25 12:55:35 +0530

    init

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel CRUD</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet" id="bootstrap-css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
